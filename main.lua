018ad0e524ce8c0dfcafbf56dd454885af67c516
Isaac Ballone <balloneij@msoe.edu>
2021-02-04 15:16:58 -0600

    Pull out the train into its own functions to make collaborating easier. Rotate around the center back of the train

--- main.lua
+++ main.lua
@@ -1,18 +1,43 @@
 PI = 3.1415926535
 
 
+Train = {
+    image = love.graphics.newImage("train.png"),
+    x = 0,
+    y = 0,
+    accel = 0.8,
+    vel = 0,
+    angle = 0
+}
+
+function Train:update(dt)
+    -- Update velocity and position
+    self.vel = self.vel + self.accel * dt
+    self.x = self.x + math.cos(self.angle) * self.vel * dt;
+    self.y = self.y + math.sin(self.angle) * self.vel * dt;
+
+    -- Update train angle based off of user input
+    if love.keyboard.isDown("d") then
+        self.angle = self.angle + (PI / 8) * dt
+    end
+    if love.keyboard.isDown("a") then
+        self.angle = self.angle - (PI / 8) * dt
+    end
+end
+
+function Train:draw()
+    love.graphics.draw(self.image,
+        self.x, self.y,
+        self.angle,
+        1, 1, -- Scaling factor
+        0, 24
+    )
+end
+
 -- Load some default values for our rectangle.
 function love.load()
     love.window.setMode(1280, 720, {})
 
-    train = {
-        image = love.graphics.newImage("train.png"),
-        x = 0,
-        y = 0,
-        accel = 0.8,
-        vel = 0,
-        angle = 0
-    }
 
     x, y, w, h = 20, 20, 60, 20  -- Note that these are global
 
@@ -39,30 +64,11 @@ function love.load()
     print(teamExplicit[2]) -- Prints Grace
 
 end
-
-function updateTrain(dt)
-    -- Update velocity and position
-    train.vel = train.vel + train.accel * dt
-    train.x = train.x + math.cos(train.angle) * train.vel * dt;
-    train.y = train.y + math.sin(train.angle) * train.vel * dt;
-
-    -- Update train angle based off of user input
-    if love.keyboard.isDown("d") then
-        train.angle = train.angle + (PI / 8) * dt
-    end
-    if love.keyboard.isDown("a") then
-        train.angle = train.angle - (PI / 8) * dt
-    end
-end
  
 function love.update(dt)
-    updateTrain(dt)
-end
-
-function drawTrain()
-    love.graphics.draw(train.image, train.x, train.y, train.angle)
+    Train:update(dt)
 end
  
 function love.draw()
-    drawTrain()
+    Train:draw()
 end
