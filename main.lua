7ae325fb11f5c9034d3f85320aa8ea0e115991e4
Isaac Ballone <balloneij@msoe.edu>
2021-02-04 12:49:31 -0600

    Demo

--- main.lua
+++ main.lua
@@ -1,6 +1,30 @@
 -- Load some default values for our rectangle.
 function love.load()
-    x, y, w, h = 20, 20, 60, 20
+    love.window.setMode(1280, 720, {})
+    x, y, w, h = 20, 20, 60, 20  -- Note that these are global
+
+    local z = 69 -- This is local
+
+    gameState = { -- This is a table
+        dog = "cat"
+    }
+
+    print(gameState.dog) -- Prints "cat"
+    print(gameState["dog"]) -- Also prints "cat"
+
+    -- For loop
+    team = {"Stuart", "Grace", "Andy"}
+    for i = 1, 3 do
+        print(team[i])
+    end
+    
+    -- Remember that hashmaps are the only data structure,
+    -- so these two are identical
+    team = {"Stuart", "Grace", "Andy"}
+    teamExplicit = {[1]="Stuart", [2]="Grace", [3]="Andy"}
+
+    print(teamExplicit[2]) -- Prints Grace
+
 end
  
 -- Increase the size of the rectangle every frame.
